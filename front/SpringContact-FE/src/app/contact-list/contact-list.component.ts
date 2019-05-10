cab8e77342cbc5e075820019a965a574d67f3c8f
DonnieCnd <donnie.ccj@gmail.com>
2019-05-10 14:29:52 +0200

    ++

--- front/SpringContact-FE/src/app/contact-list/contact-list.component.ts
+++ front/SpringContact-FE/src/app/contact-list/contact-list.component.ts
@@ -1,19 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
   styleUrls: ['./contact-list.component.css']
 })
+
 export class ContactListComponent implements OnInit {
 
   constructor(private httpClient: HttpClient) { }
 
-  contacts: any;
+  url: string = "assets/mocks/myJSONfile.json";
+  data: any;
+  contacts = {};
+  finalContacts = [];
+  contactsProps;
 
   ngOnInit() {
-    this.httpClient.get('http://localhost:8081/contacts').subscribe(result => console.log(result));
+    this.httpClient.get(this.url).subscribe(result => {
+      this.data = result;
+      this.data = this.data.contacts;
+      this.filterContacts(this.data);
+    })
   }
 
+  filterContacts(data){
+    for(let contact of data){
+
+      const letter = contact.lastName.charAt(0);
+     
+      if(!this.contacts[letter])
+        this.contacts[letter] = [];
+     
+      this.contacts[letter].push(contact);
+    }
+    console.log(this.contacts);
+    this.contactsProps = Object.keys(this.contacts);
+    console.log(this.contactsProps);
+    for(let prop of this.contactsProps){
+    this.finalContacts.push(this.contacts[prop])
+     
+    }
+    console.log(this.finalContacts)
+  }
 }
