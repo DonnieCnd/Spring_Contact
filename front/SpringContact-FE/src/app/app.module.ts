a2077ce4fa6005763054731a68e2c639f12b79d6
DonnieCnd <donnie.ccj@gmail.com>
2019-06-03 12:03:49 +0200

    ++

--- front/SpringContact-FE/src/app/app.module.ts
+++ front/SpringContact-FE/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { ContactListComponent } from './contact/contact-list/contact-list.compon
 import { HttpClientModule } from '@angular/common/http';
 import { TooltipModule, BsModalRef, ModalModule, TypeaheadModule } from 'ngx-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { AutofocusDirective } from './autofocus.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ContactListComponent,
+    AutofocusDirective,
     
   ],
   imports: [
